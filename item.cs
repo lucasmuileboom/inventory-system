f0576485ba6692bc79e344376ebfbbb309b21d79
lucasmuileboom <muileboomlucas@gmail.com>
2018-11-01 21:59:39 +0100

    first commit

---
+++ item.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class item : MonoBehaviour
+{
+    [SerializeField]private int maxAmountOfItems;
+    private int amoutOfItems = 0;
+    public int AmoutOfItems
+    {
+        get
+        {
+            return amoutOfItems;
+        }
+        set
+        {
+            if (amoutOfItems + value > maxAmountOfItems)
+            {
+                Drop((value + amoutOfItems)-maxAmountOfItems);
+                amoutOfItems = maxAmountOfItems;
+                print(maxAmountOfItems);
+                return;
+            }
+            amoutOfItems += value;
+            print(amoutOfItems);
+        }
+    }
+
+    public abstract void Use();
+    public abstract void Drop(int amount);
+}
---
+++ itemtemp.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemtemp : item
+{
+    public override void Use()
+    {
+        print("I have been used");
+    }
+
+    public override void Drop(int amount)
+    {
+        print("drop:"+ amount);
+        //Destroy(this.gameObject);
+    }
+}
